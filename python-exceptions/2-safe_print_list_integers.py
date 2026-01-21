4c01b5724f07e21f067d761bb82589ea542b0886
Micael Velez <mvrod2314@gmail.com>
2026-01-21 18:36:25 +0000

    pntlsintg

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,10 +5,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (ValueError, TypeError):
-            continue
         except IndexError:
             break
-    if count > 0 or x > 0:
-        print()
+        except (ValueError, TypeError):
+            continue
+    print()
     return count
