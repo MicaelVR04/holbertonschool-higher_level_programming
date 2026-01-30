cedeb7c43c92b1d23283a5e9d10a45c6ecfca79d
Micael Velez <mvrod2314@gmail.com>
2026-01-30 17:48:34 +0000

    1-sqr

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class with a private size attribute."""
+
+
+class Square:
+    """This class represents a square defined by its size."""
+
+    def __init__(self, size):
+        """Initialize a new Square with a given size."""
+        self.__size = size
