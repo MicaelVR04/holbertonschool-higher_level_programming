60a67836f6eac9ef5b63c9c89b7263006f53619f
Micael Velez <mvrod2314@gmail.com>
2026-02-16 00:52:43 +0000

    load

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Create a Python object from a JSON file."""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Load and return a Python object from a JSON file."""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        return json.load(f)
