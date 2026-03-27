f1cc8e519366bf17b980e1ffdc791ea260287e96
Micael Velez <mvrod2314@gmail.com>
2026-03-27 14:37:51 +0000

    sc5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,3 @@
+document.querySelector('#update_header').addEventListener('click', function () {
+  document.querySelector('header').textContent = 'New Headercd /root/holbertonschool-higher_level_programming/javascript-dom_manipulation' '#!/usr/bin/node' 'exports.add = function (a, b) {' '  return a + b;' '};' > 13-add.js;
+});
